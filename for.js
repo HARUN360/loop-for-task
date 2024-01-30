580b42eb44afa668133043723b1d1e39f88d4cee
HARUN360 <mdharun573192@gmail.com>
2024-01-30 15:00:41 +0600

    for loop

--- for.js
+++ for.js
@@ -1,3 +1,57 @@
-for(let i = 0; i<10; i++){
-    console.log(i);
+// 1."I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+// for(let i = 1; i<=60; i++){
+//     console.log('I will invest at least 6 hrs every single day for next 60 days! = ' + i );
+// }
+
+
+
+// 2. Find all the odd numbers from 61 to 100.
+// for(let i = 61; i<=100;i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+// 3. Find all the even numbers from 78 to 98.
+// for(let i = 61; i<=100;i++){
+//         if(i%2!==0){
+//             console.log(i);
+//         }
+//      }
+
+
+
+// 4. Display sum of all the odd numbers from 91 to 129.
+// let sum = 0;
+// for(let i = 91; i<=129;i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+//     sum = sum+i;
+// }
+// console.log("the summatin of odd number from 91 to 129 = " + sum);
+
+//5. Display sum of all the even numbers from 51 to 85.
+// let sum = 0;
+// for(let i = 51; i<=85;i++){
+//     if(i%2!==0){
+//         console.log(i);
+//     }
+//     sum = sum+i;
+// }
+// console.log("the summatin of even number from 51 to 85 = " + sum);
+
+
+
+
+// 6.Generate a multiplication table for number 9
+// for(let i = 9; i<=90; i = i+9){
+//     console.log('9gore namata = ' + i);
+// }
+
+
+
+// 7.Implement a countdown timer that counts down from 81 to 65.
+for(let i = 81; i>=65; i = i-1){
+    console.log('counts down from 81 to 65 = ' + i);
 }
