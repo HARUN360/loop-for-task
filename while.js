566bda291f5e3365a5ce2811f6cfcb79420beede
HARUN360 <mdharun573192@gmail.com>
2024-01-30 14:44:26 +0600

    while loop

---
+++ while.js
@@ -0,0 +1,65 @@
+//1. "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+/*
+let i = 1;
+while(i<=60){
+    console.log("I will invest at least 6 hrs every single day for next 60 days! = " + i);
+    i++
+}
+*/
+
+//2.Find all the odd numbers from 61 to 100.
+/* let i = 61;
+ while(i<=100){
+    if(i%2==0){
+        console.log("it is a odd nummver = " + i);
+    }
+    i++
+}
+*/
+
+//3. Find all the even numbers from 78 to 98.
+/* let i = 78;
+ while(i<=98){
+    if(i%2 !== 0){
+        console.log("it is a even nummver = " + i);
+    }
+    i++
+}
+*/
+
+//4. Display sum of all the odd numbers from 81 to 131.
+// let i = 81;
+//  while(i<=131){
+//     if(i%2==0){
+//         console.log("it is a odd nummver = " + i);
+//     }
+//     i++
+// }
+
+
+//5. Display sum of all the even numbers from 206 to 311.
+// let i = 206;
+// let sum = 0;
+//  while(i<=311){
+//     sum = sum+i;
+//     if(i%2 !== 0){
+//         console.log("it is a even nummver = " + i);
+//     }
+//     i++
+
+// }
+// console.log("the summation = " , sum);
+
+// 6. As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+// let i = 5;
+// while(i<=50){
+//     console.log("the multiplacatin 5 = " + i);
+//   i = i+5;
+// }
+
+// 7. Implement a countdown timer that counts down from 21 to 15.
+let i = 21;
+while(i >= 15){
+    console.log("the count of 21 to 15 = " + i);
+    i = i-1;
+}
